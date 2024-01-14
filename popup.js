39a9ae46092a95a543c6be44e146814fac12c512
Megakuul <megakuul@gmx.ch>
2024-01-14 12:26:49 +0100

    Updated design

--- popup.js
+++ popup.js
@@ -2,7 +2,13 @@ document.addEventListener("DOMContentLoaded", function() {
 		const enablebtn = document.getElementById("enable-btn");
 		
 		chrome.storage.local.get(["enabled"], function(result) {
-				enablebtn.innerHTML = result.enabled ? "Disable" : "Enable";
+				if (result.enabled) {
+						enablebtn.innerHTML = "Disable";
+						enablebtn.classList.add("enabled");
+				} else {
+						enablebtn.innerHTML = "Enable";
+						enablebtn.classList.remove("enabled");
+				}
 		});
 
 		enablebtn.addEventListener("click", function() {
